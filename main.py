c66e7721e4a2e85f93677ce9da28173ebfce898e
Peter <peter.matheny@gmail.com>
2017-03-03 15:02:03 -0600

    Fixed the plotting

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ for e in es:
     plt.plot(x, y)
     plt.xlabel('meters')
     plt.ylabel('meters')
-
+plt.show()
 # read in azimuth data
 allsats = ['G{:02}'.format(num) for num in range(1, 33)]
 azimuths = dict([])
@@ -100,3 +100,66 @@ ax.set_rmax(90.0)
 ax.set_yticks(range(0, 90, 10))    # (min int, max int, increment)
 ax.set_yticklabels(map(str, range(90, 0, -10)))
 plt.title('ROB4-G01 1/11/2017')
+plt.show()
+
+key = 'G01'
+snrvsele = [(np.sin(np.radians(x[1])), y[1]) for
+            x, y in zip(elev[key], snr[key])]
+time = [x[0] for x in snr[key] if 3e4 < x[0] < 6e4]
+# print(min(time),max(time),(max(time)-min(time))/2)
+snrcut = [x for x in snr[key] if
+          min(time) <= x[0] <= min(time) + (max(time) - min(time)) / 2]
+elecut = [x for x in elev[key] if
+          min(time) <= x[0] <= min(time) + (max(time) - min(time)) / 2]
+snrg01 = [x[1] for x in snrcut]
+eleg01 = [x[1] for x in elecut]
+
+# isolate the lower level of the track
+testcut = [[x, y] for x, y in zip(eleg01, snrg01) if float(x) < 30]
+el = [x[0] for x in testcut]
+sn = [x[1] for x in testcut]
+
+# convert from dbhz to volts
+snvolts = np.array([10 ** (x / 20) for x in sn])
+
+# remove the direct power
+model = np.polyfit(el, snvolts, 2)
+predicted = np.polyval(model, el)
+snvolts = snvolts - predicted
+
+# detrend and convert to radians
+snvolts = signal.detrend(snvolts)
+el = np.sin(np.radians(el))
+
+
+plt.scatter(el, snvolts, linewidths=.05, s=1)
+plt.xlabel('Elevation (sin(e))')
+plt.ylabel('Volts')
+plt.show()
+
+f = np.linspace(0.01, 1500, 2000)
+
+pgram = signal.lombscargle(np.array(el), np.array(snvolts), f)
+plt.figure()
+ax = plt.subplot(111)
+ax.plot(f, pgram)
+plt.xlabel('Frequency 1/sin(e)')
+plt.ylabel('Spectral Amp Volts')
+plt.show()
+
+phase = (max(*(zip(pgram, f))))
+print(phase)
+reflection = 4.28018 * np.cos(114.07 * el)
+new_sn = snvolts-reflection
+plt.figure()
+ax = plt.subplot(111)
+plt.scatter(el, new_sn, s=1, linewidths=0.05)
+plt.show()
+
+pgram = signal.lombscargle(np.array(el), np.array(new_sn), f)
+plt.figure()
+ax = plt.subplot(111)
+ax.plot(f, pgram)
+plt.xlabel('Frequency 1/sin(e)')
+plt.ylabel('Spectral Amp Volts')
+plt.show()
